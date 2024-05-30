2cb4258cac950dae62ae446b373391e6fde6f894
MarianaEstaTrabajando <l23121131@morelia.tecnm.mx>
2024-05-30 00:33:35 -0600

    Ajuste

--- src/Usuarios/Alumno.java
+++ src/Usuarios/Alumno.java
@@ -31,6 +31,14 @@ public class Alumno extends Usuario{
         return String.format("%s, Semestre: %d, Promedio: %f, Numero de Control: %s", super.toString(), semestre, promedio, numControl);
     }
 
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+
     public int getSemestre() {
         return semestre;
     }
@@ -89,4 +97,139 @@ public class Alumno extends Usuario{
         System.out.println(">Alumno registrado<");
 
     }
+    public static void modificarAlumno() {
+        Scanner sc = new Scanner(System.in);
+        mostrarAlumnos();
+        System.out.println("Selecciona al alumno: ");
+        int numAlumno = pedirAlumno();
+        int opt = 10;
+        do {
+            System.out.println("¿Qué información deseas editar?");
+            System.out.println("1) Nombre\n2) Apellidos \n3) Ciudad\n4) Estado\n5) Dirección\n6) Fecha de nacimiento\n 7)Contraseña\n 0)Salir/Regresar");
+            opt = DatosComun.pedirNumero();
+            Alumno alumno = (Alumno) Sistema.usuarios.get(Rol.ALUMNO).get(numAlumno - 1);
+            switch (opt) {
+                case 1:
+                    System.out.println("Ingrese el nuevo nombre: ");
+                    alumno.setNombre(DatosComun.pedirDatoString());
+                    Sistema.usuarios.get(Rol.ALUMNO).set(numAlumno - 1, alumno);
+                    String curpAntigua = alumno.getCurp();
+                    char sexo = curpAntigua.charAt(10);
+                    String nuevacurp = Generador.generarCURP(alumno.getNombre(), alumno.getApellidoPaterno(), alumno.getApellidoMaterno(), alumno.getFechaNacimiento(), sexo, alumno.getEstado());
+
+                    alumno.setCurp(nuevacurp);
+                    System.out.println("Nombre modificado");
+                    break;
+
+                case 2:
+                    System.out.println("Ingrese el nuevo apellido Paterno: ");
+                    alumno.setApellidoPaterno(DatosComun.pedirDatoString());
+                    System.out.println("Ingrese el nuevo apellido Materno: ");
+                    alumno.setApellidoMaterno(DatosComun.pedirDatoString());
+                    String curpAntigua1 = alumno.getCurp();
+                    char sexo1 = curpAntigua1.charAt(10);
+                    String nuevaCurp = Generador.generarCURP(alumno.getNombre(), alumno.getApellidoPaterno(), alumno.getApellidoMaterno(), alumno.getFechaNacimiento(), sexo1, alumno.getEstado());
+
+                    alumno.setCurp(nuevaCurp);
+                    Sistema.usuarios.get(Rol.ALUMNO).set(numAlumno - 1, alumno);
+                    System.out.println("Apellido modificado");
+                    break;
+
+                case 3:
+                    System.out.println("Ingrese nueva ciudad: ");
+                    alumno.setCiudad(DatosComun.pedirDatoString());
+                    Sistema.usuarios.get(Rol.ALUMNO).set(numAlumno - 1, alumno);
+                    System.out.println("Ciudad actualizada");
+                    break;
+
+                case 4:
+                    System.out.println("Ingrese nuevo estado: ");
+                    alumno.setEstado(DatosComun.pedirDatoString());
+                    Sistema.usuarios.get(Rol.ALUMNO).set(numAlumno - 1, alumno);
+                    System.out.println("Estado actualizado");
+                    break;
+
+                case 5:
+                    System.out.println("Ingrese nueva direccion: ");
+                    alumno.setDireccion(DatosComun.pedirDireccion());
+                    Sistema.usuarios.get(Rol.ALUMNO).set(numAlumno - 1, alumno);
+                    System.out.println("Dirección actualizada");
+                    break;
+
+                case 6:
+                    System.out.println("Fecha de nacimiento");
+                    LocalDate nuevaFechaNacimiento = DatosComun.obtenerFechaNacimiento();
+                    alumno.setFechaNacimiento(nuevaFechaNacimiento);
+                    int anioNacimiento = nuevaFechaNacimiento.getYear();
+                    alumno.setAnioNacimiento(anioNacimiento);
+                    String curpAntigua2 = alumno.getCurp();
+                    char sexo2 = curpAntigua2.charAt(10);
+                    String curpNueva2 = Generador.generarCURP(alumno.getNombre(), alumno.getApellidoPaterno(), alumno.getApellidoMaterno(), alumno.getFechaNacimiento(), sexo2, alumno.getEstado());
+
+                    alumno.setCurp(curpNueva2);
+                    System.out.println("Fecha Nacimiento Actualizada");
+                    break;
+
+                case 7:
+                    System.out.println("Ingrese nueva contraseña");
+                    String nuevaContrasena = sc.nextLine();
+                    alumno.setContrasena(nuevaContrasena);
+                    System.out.println("Contrasena Actualizada");
+                    break;
+                case 0:
+                    System.out.println("Usted ha salido de modificar alumno. ");
+                    //UsuarioEnSesion.getInstancia().cerrarSesion();
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + opt);
+
+            }
+
+        } while (opt != 0);
+
+    }
+
+    private static int pedirAlumno() {
+        Scanner sc = new Scanner(System.in);
+        boolean confirmacion = false;
+        int numAlumno = 0;
+
+        do {
+            confirmacion = false;
+            try {
+                System.out.println("Selecciona al alumno: ");
+                numAlumno = DatosComun.pedirNumero();
+
+                if (numAlumno < 1 || numAlumno > Sistema.usuarios.get(Rol.ALUMNO).size()) {
+                    throw new IndexOutOfBoundsException("El dato ingresado está fuera del tamaño de la lista");
+                } else {
+                    return numAlumno;
+                }
+            } catch (IndexOutOfBoundsException error) {
+                System.out.println("Error: " + error.getMessage());
+
+            }
+        } while (confirmacion);
+        sc.nextLine();
+        return numAlumno;
+    }
+
+    public static void mostrarAlumnos() {
+        System.out.println("\nAlumnos: \n");
+        if (Sistema.usuarios.get(Rol.ALUMNO).isEmpty()) {
+            System.out.println("No hay alumnos registrados");
+        } else {
+            for (int i = 0; i < Sistema.usuarios.get(Rol.ALUMNO).size(); i++) {
+                Alumno alumno = (Alumno) Sistema.usuarios.get(Rol.ALUMNO).get(i);
+                System.out.println((i + 1) + " " + alumno.toString());
+            }
+        }
+    }
+
+    public static void mostrarAlmunosPorFiltro(){
+
+
+    }
+
+
 }
--- src/Usuarios/Profesor.java
+++ src/Usuarios/Profesor.java
@@ -1,5 +1,6 @@
 package Usuarios;
 
+import Secciones.utils.NombreCarrera;
 import Usuarios.utils.DatosComun;
 import Usuarios.utils.Rol;
 import mindbox.Sistema;
@@ -7,17 +8,19 @@ import mindbox.UsuarioEnSesion;
 import mindbox.utils.Generador;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Profesor extends Trabajador {
     private String numControl;
 
-    public Profesor(String nombre, String apellidoPaterno, String apellidoMaterno, int anioNacimiento, LocalDate fechaNacimiento, String ciudad, String estado, String direccion, String curp, LocalDate fechaRegistro, String usuario, String contrasena, Rol rol, String rfc, double salario, String numControl){
+    public Profesor(String nombre, String apellidoPaterno, String apellidoMaterno, int anioNacimiento, LocalDate fechaNacimiento, String ciudad, String estado, String direccion, String curp, LocalDate fechaRegistro, String usuario, String contrasena, Rol rol, String rfc, double salario, String numControl) {
         super(nombre, apellidoPaterno, apellidoMaterno, anioNacimiento, fechaNacimiento, ciudad, estado, direccion, curp, fechaRegistro, usuario, contrasena, rol, rfc, salario);
         this.numControl = numControl;
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("%s, numero de Control: %s", super.toString(), numControl);
     }
 
@@ -28,44 +31,81 @@ public class Profesor extends Trabajador {
     public void setNumControl(String numControl) {
         this.numControl = numControl;
     }
+
     ////////////
-    public void darDeAlta(){
+    public void darDeAlta() {
 
     }
-    private void verInformacion(){
+
+    private void verInformacion() {
 
     }
-    public void actualizarInformacion(){
+
+    public void actualizarInformacion() {
 
     }
-    private void verGrupos(){
+
+    private void verGrupos() {
 
     }
-    private void asignarCalificacion(){
+
+    private void asignarCalificacion() {
 
     }
-    private void modificarCalificacion(){
+
+    private void modificarCalificacion() {
 
     }
-    public static void modificarProfesor(){
-        Scanner sc=new Scanner(System.in);
+
+    public static void registrarProfesor(NombreCarrera carrera) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> datosComun = DatosComun.registrarDatosComun(Rol.PROFESOR);
+        String nombre = datosComun.get(0);
+        String apellidoPaterno = datosComun.get(1);
+        String apellidoMaterno = datosComun.get(2);
+        String anioNacimiento = datosComun.get(3);
+        String fechaNacimientoCadena = datosComun.get(4);
+        String ciudad = datosComun.get(5);
+        String estado = datosComun.get(6);
+        String direccion = datosComun.get(7);
+        String CURP = datosComun.get(8);
+        String fechaRegistroCadena = datosComun.get(9);
+        String nombreUsuario = datosComun.get(10);
+        String contrasena = datosComun.get(11);
+        LocalDate fechaRegistro = LocalDate.parse(fechaRegistroCadena);
+        String numContol = Generador.generarNumControl(Rol.PROFESOR, carrera, nombre, fechaRegistro);
+        //ocupo volver int el año de nacimiento y LocalDate la fecha de nacimiento
+        int anioNacimientoint = Integer.parseInt(anioNacimiento);
+        LocalDate fechaNacimiento = LocalDate.parse(fechaNacimientoCadena);
+        //
+        System.out.println("Ingrese el salario. ");
+        double salario = DatosComun.pedirValorDouble();
+        String rfc = Generador.generarRFC(nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento);
+        Profesor profesor = new Profesor(nombre, apellidoPaterno, apellidoMaterno, anioNacimientoint, fechaNacimiento, ciudad, estado, direccion, CURP, fechaRegistro, nombreUsuario, contrasena, Rol.PROFESOR, rfc, salario, numContol);
+        Sistema.usuarios.get(Rol.PROFESOR).add(profesor);
+        System.out.println(">Profesor registrado<");
+
+    }
+
+    public static void modificarProfesor() {
+        Scanner sc = new Scanner(System.in);
         mostrarProfesores();
-        System.out.println("Selecciona el profesor: ");
+        System.out.println("Selecciona al profesor: ");
         int numProfesor = pedirProfesor();
-        int opt =10;
-        do{
+        int opt = 10;
+        do {
             System.out.println("¿Qué información deseas editar?");
             System.out.println("1) Nombre\n2) Apellidos \n3) Ciudad\n4) Estado\n5) Dirección\n6) Fecha de nacimiento\n 7)Contraseña\n 0)Salir/Regresar");
-            opt= DatosComun.pedirNumero();
-            Profesor profesor = (Profesor) Sistema.usuarios.get(Rol.PROFESOR).get(numProfesor-1);
-            switch (opt){
+            opt = DatosComun.pedirNumero();
+            Profesor profesor = (Profesor) Sistema.usuarios.get(Rol.PROFESOR).get(numProfesor - 1);
+            switch (opt) {
                 case 1:
                     System.out.println("Ingrese el nuevo nombre: ");
                     profesor.setNombre(DatosComun.pedirDatoString());
-                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor-1,profesor);
+                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor - 1, profesor);
                     String curpAntigua = profesor.getCurp();
                     char sexo = curpAntigua.charAt(10);
-                    String nuevacurp= Generador.generarCURP(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento(), sexo, profesor.getEstado());
+                    String nuevacurp = Generador.generarCURP(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento(), sexo, profesor.getEstado());
                     String nuevorfc = Generador.generarRFC(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento());
                     profesor.setRfc(nuevorfc);
                     profesor.setCurp(nuevacurp);
@@ -79,36 +119,36 @@ public class Profesor extends Trabajador {
                     profesor.setApellidoMaterno(DatosComun.pedirDatoString());
                     String curpAntigua1 = profesor.getCurp();
                     char sexo1 = curpAntigua1.charAt(10);
-                    String nuevaCurp= Generador.generarCURP(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento(), sexo1, profesor.getEstado());
+                    String nuevaCurp = Generador.generarCURP(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento(), sexo1, profesor.getEstado());
                     String nuevorfc1 = Generador.generarRFC(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento());
                     profesor.setRfc(nuevorfc1);
                     profesor.setCurp(nuevaCurp);
-                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor-1,profesor);
+                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor - 1, profesor);
                     System.out.println("Apellido modificado");
                     break;
 
                 case 3:
                     System.out.println("Ingrese nueva ciudad: ");
                     profesor.setCiudad(DatosComun.pedirDatoString());
-                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor-1,profesor);
+                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor - 1, profesor);
                     System.out.println("Ciudad actualizada");
                     break;
 
                 case 4:
                     System.out.println("Ingrese nuevo estado: ");
                     profesor.setEstado(DatosComun.pedirDatoString());
-                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor-1,profesor);
+                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor - 1, profesor);
                     System.out.println("Estado actualizado");
                     break;
 
                 case 5:
                     System.out.println("Ingrese nueva direccion: ");
                     profesor.setDireccion(DatosComun.pedirDireccion());
-                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor-1,profesor);
+                    Sistema.usuarios.get(Rol.PROFESOR).set(numProfesor - 1, profesor);
                     System.out.println("Dirección actualizada");
                     break;
 
-                case 6 :
+                case 6:
                     System.out.println("Fecha de nacimiento");
                     LocalDate nuevaFechaNacimiento = DatosComun.obtenerFechaNacimiento();
                     profesor.setFechaNacimiento(nuevaFechaNacimiento);
@@ -116,65 +156,65 @@ public class Profesor extends Trabajador {
                     profesor.setAnioNacimiento(anioNacimiento);
                     String curpAntigua2 = profesor.getCurp();
                     char sexo2 = curpAntigua2.charAt(10);
-                    String curpNueva2= Generador.generarCURP(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento(), sexo2, profesor.getEstado());
+                    String curpNueva2 = Generador.generarCURP(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento(), sexo2, profesor.getEstado());
                     String RFCNuevo2 = Generador.generarRFC(profesor.getNombre(), profesor.getApellidoPaterno(), profesor.getApellidoMaterno(), profesor.getFechaNacimiento());
                     profesor.setRfc(RFCNuevo2);
                     profesor.setCurp(curpNueva2);
                     System.out.println("Fecha Nacimiento Actualizada");
                     break;
 
-                case 7 :
+                case 7:
                     System.out.println("Ingrese nueva contraseña");
                     String nuevaContrasena = sc.nextLine();
                     profesor.setContrasena(nuevaContrasena);
                     System.out.println("Contrasena Actualizada");
                     break;
-                case 0 :
-                    System.out.println("Tenemos que poner un método mostrarMenuEmpleado");
-                    UsuarioEnSesion.getInstancia().cerrarSesion();
+                case 0:
+                    System.out.println("Usted ha salido de modificar profesor. ");
+                    //UsuarioEnSesion.getInstancia().cerrarSesion();
                     break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + opt);
 
             }
 
-        }while(opt!=0);
+        } while (opt != 0);
 
     }
 
-    private static int pedirProfesor(){
+    private static int pedirProfesor() {
         Scanner sc = new Scanner(System.in);
         boolean confirmacion = false;
-        int numProfesor=0;
+        int numProfesor = 0;
 
-        do{
+        do {
             confirmacion = false;
-            try{
-                System.out.println("Selecciona el ejecutivo: ");
-                numProfesor=DatosComun.pedirNumero();
+            try {
+                System.out.println("Selecciona al profesor: ");
+                numProfesor = DatosComun.pedirNumero();
 
-                if(numProfesor<1||numProfesor>Sistema.usuarios.get(Rol.PROFESOR).size()){
+                if (numProfesor < 1 || numProfesor > Sistema.usuarios.get(Rol.PROFESOR).size()) {
                     throw new IndexOutOfBoundsException("El dato ingresado está fuera del tamaño de la lista");
+                } else {
+                    return numProfesor;
                 }
-                else{
-                    return  numProfesor;
-                }
-            }catch(IndexOutOfBoundsException error){
-                System.out.println("Error: "+ error.getMessage());
+            } catch (IndexOutOfBoundsException error) {
+                System.out.println("Error: " + error.getMessage());
 
             }
-        }while(confirmacion);
+        } while (confirmacion);
         sc.nextLine();
-        return  numProfesor;
+        return numProfesor;
     }
-    public static void mostrarProfesores(){
-        System.out.println("\nEjecutivos en el banco\n");
+
+    public static void mostrarProfesores() {
+        System.out.println("\nProfesores: \n");
         if (Sistema.usuarios.get(Rol.PROFESOR).isEmpty()) {
-            System.out.println("No hay ejecutivos registrados");
+            System.out.println("No hay profesores registrados");
         } else {
-            for(int i=0;i<Sistema.usuarios.get(Rol.PROFESOR).size();i++){
-                Profesor profesor=(Profesor)Sistema.usuarios.get(Rol.PROFESOR).get(i);
-                System.out.println(profesor.toString());
+            for (int i = 0; i < Sistema.usuarios.get(Rol.PROFESOR).size(); i++) {
+                Profesor profesor = (Profesor) Sistema.usuarios.get(Rol.PROFESOR).get(i);
+                System.out.println((i + 1) + " " + profesor.toString());
             }
         }
     }
