1c4dfc3b093acee5c03baefeb4dff0a1ad6a3c2a
MarianaEstaTrabajando <l23121131@morelia.tecnm.mx>
2024-05-31 22:46:26 -0600

    Ajuste

--- src/Usuarios/utils/Calificacion.java
+++ src/Usuarios/utils/Calificacion.java
@@ -1,38 +1,83 @@
 package Usuarios.utils;
+
 import Secciones.Materia;
 import Usuarios.Alumno;
+import Usuarios.Profesor;
 import Usuarios.Usuario;
 import mindbox.Sistema;
 import mindbox.UsuarioEnSesion;
 
+
+
 public class Calificacion {
-    int calificacion;
-    boolean aprobado=false;
+    double calificacion;
+    boolean aprobado = false;
     Materia materia;
 
     //, materia
-    public Calificacion(int calificacion) {
-        this.calificacion=calificacion;
-        if(calificacion>=70){
-            aprobado=true;
+    public Calificacion(double calificacion, Materia materia) {
+        this.calificacion = calificacion;
+        if (calificacion >= 70) {
+            aprobado = true;
         }
-    }
-
-    public static void registrarCalificacion(){
-        for (Usuario usuario : Sistema.usuarios.get(Rol.PROFESOR)) {
-          //  if(UsuarioEnSesion.getInstancia().getUsuarioActual()==OBTENER A PROFESOR DE ALGUN GRUPO){
-
-            //  }
-
-
-
-        }
+        this.materia = materia;
 
     }
 
-    public static void modificarCalificacion(){
-        System.out.println("Escoja al alumno: ");
+    public static void registrarCalificacion() {
+       int mat=Profesor.numDeMateria();
+       int alum=0;
+       System.out.println("Alumnos: ");
+       Profesor.mostrarAlumnosMateria(mat);
+
+        while (alum < 1 || alum >= ((Profesor) UsuarioEnSesion.getInstancia().getUsuarioActual()).getMaterias().get(mat).getGrupo().getAlumnos().size()) {
+            System.out.println("Ingrese al alumno: ");
+            alum = DatosComun.pedirNumero();
+        }
+        alum--;
+        double cali=-4;
+        while (cali<0||cali>100) {
+            System.out.println("Ingresar Calificación: ");
+             cali= DatosComun.pedirValorDouble();
+        }
+        Calificacion calificacion= new Calificacion (cali,((Profesor) UsuarioEnSesion.getInstancia().getUsuarioActual()).getMaterias().get(mat));
+        for(Usuario usuario: Sistema.usuarios.get(Rol.ALUMNO)){
+            if(((Alumno) usuario).getNumControl().equals(((Profesor) UsuarioEnSesion.getInstancia().getUsuarioActual()).getMaterias().get(mat).getGrupo().getAlumnos().get(alum).getNumControl())){
+               /* ((Alumno)usuario).getCalificaciones().add(calificacion);*/
+            }
+        }
+
+    }
+
+    public static void modificarCalificacion() {
+
         //buscar alumno, verificando que el maestro en sesion tenga a ese alumno
 
     }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public boolean isAprobado() {
+        return aprobado;
+    }
+
+    public void setAprobado(boolean aprobado) {
+        this.aprobado = aprobado;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+
 }
