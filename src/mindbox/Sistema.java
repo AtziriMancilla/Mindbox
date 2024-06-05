468746162c9ce0102b0f77a878313aed464dafcc
Atziri Mancilla <atzirimc22@gmail.com>
2024-06-05 10:25:03 -0600

    matamos grupo

--- src/mindbox/Sistema.java
+++ src/mindbox/Sistema.java
@@ -111,13 +111,6 @@ public class Sistema {
         Grupo.inicializarMaterias(g1);
         Grupo.addMateriasSemestre(g1);
         grupos.add(g1);
-
-//        Grupo g2 = new Grupo(NombreCarrera.ELC, 3, TipoGrupo.A);
-//        grupos.add(g2);
-//        semestres.get(2).getGrupos().add(g2);
-//        Grupo.inicializarMaterias(g2);
-//        Grupo.addMateriasSemestre(g2);
-
     }
     //Setter para poder asignar los nuevos datos obtenidos del json al atributo HashMap usuarios.
     public static void setUsuarios(HashMap<Rol, ArrayList<Usuario>> usuarios) {
