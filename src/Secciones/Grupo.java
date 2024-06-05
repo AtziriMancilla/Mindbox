fdb92a2baf6b6d152a54ac4caf5bb3e685da0649
sianya <sianyidgm@gmail.com>
2024-06-05 00:15:40 -0600

    mod prof

--- src/Secciones/Grupo.java
+++ src/Secciones/Grupo.java
@@ -423,9 +423,10 @@ public class Grupo {
         System.out.println("Modificar Materia (Profesor en materia)\n");
         // El unico atributo modificable es profesor
         Profesor.mostrarProfesores();
-        Profesor profesor = (Profesor) Sistema.usuarios.get(Rol.PROFESOR).get(Profesor.pedirProfesor());
+        int idProf = Profesor.pedirProfesor();
+        Profesor profesor = (Profesor) Sistema.usuarios.get(Rol.PROFESOR).get(idProf);
         asignarProfe(materia, profesor);
-        ((Profesor) Sistema.usuarios.get(Rol.PROFESOR).get(Profesor.pedirProfesor())).asignarMaterias();
+        ((Profesor) Sistema.usuarios.get(Rol.PROFESOR).get(idProf)).asignarMaterias();
     }
 
     public static void mostrarMaterias(Grupo grupo){
